52e1276799b54a0cedb96b8d6d4e2576ce3b3bc9
jay0425 <heedev94@gmail.com>
2024-03-27 00:09:40 +0900

    redirect

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import './App.css';
 import Homepage from './page/Homepage';
 import Aboutpage from './page/Aboutpage';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import Productpage from './page/Productpage';
 import ProductDetailPage from './page/ProductDetailPage';
+import LoginPage from './page/LoginPage';
+import { useState } from 'react';
+import UserPage from './page/UserPage';
 
 function App() {
+  const [authenticate, setAthenticate] = useState(false);
+  const PrivateRoute = () => {
+    return authenticate === true ? <UserPage /> : <Navigate to="/login" />;
+  }; // Navigate => 리다이렉트 하게 도와주는 컴포넌트.
   return (
     <div>
       <Routes>
@@ -13,6 +20,8 @@ function App() {
         <Route path="/about" element={<Aboutpage />} />
         <Route path="/products" element={<Productpage />} />
         <Route path="/products/:id" element={<ProductDetailPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/user" element={<PrivateRoute />} />
       </Routes>
     </div>
   );
